2ab71459fdd8485dc292dcb4ec5f7613fcb168f5
zidisw <zidirsyadin@gmail.com>
2026-05-28 16:05:40 +0800

    fix bug expired when edit button clicked

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,14 @@
 <?php
 
+if (
+    (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') ||
+    (isset($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] === 'on') ||
+    (isset($_SERVER['HTTP_CF_VISITOR']) && str_contains($_SERVER['HTTP_CF_VISITOR'], 'https')) ||
+    (isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] === 'rodajayasakti.id' || $_SERVER['HTTP_HOST'] === 'www.rodajayasakti.id'))
+) {
+    $_SERVER['HTTPS'] = 'on';
+}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
